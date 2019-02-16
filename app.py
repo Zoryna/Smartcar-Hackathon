6987b1b1b600ab2bb263fda36fe9703c00828b4b
Paulina Adamski <padamski@luc.edu>
2019-02-16 17:37:28 -0600

    heroku

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def index():
     return render_template('www/index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+#if __name__ == '__main__':
+    #app.run(debug=True, host='0.0.0.0')
