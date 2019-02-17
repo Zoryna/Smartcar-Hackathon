f34d9c1586d9684a3c9a098215b26b27e30680db
Paulina Adamski <padamski@luc.edu>
2019-02-16 18:30:40 -0600

    First commit for heroku

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template('www/index.html')
 
-
+if __name__ == '__main__':
+    my_awesome_app.run()
 #if __name__ == '__main__':
     #app.run(debug=True, host='0.0.0.0')
