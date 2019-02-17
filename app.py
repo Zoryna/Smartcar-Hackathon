a7ed602df252a6e7796058f79d63eb8c640f3437
Paulina Adamski <padamski@luc.edu>
2019-02-16 18:45:13 -0600

    changed app name

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+smartcar_app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('www/index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    smartcar_app.run(debug=True, host='0.0.0.0')
