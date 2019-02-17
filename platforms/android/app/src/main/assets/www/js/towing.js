a8a48f1981d1999ceacdbc48df3e27101880122f
Linette V Maliakal <lmaliakal21@gmail.com>
2019-02-17 08:27:17 -0600

    connected all front-end files
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

---
+++ platforms/android/app/src/main/assets/www/js/towing.js
@@ -0,0 +1,16 @@
+function accordion(){
+
+  var acc = document.getElementsByClassName("accordion");
+
+  for (var i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.display === "block") {
+        content.display = "none";
+      } else {
+        content.display = "block";
+      }
+    });
+  }
+}
